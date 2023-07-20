bc39cb201128ebaadb9d2b06113c985275dffcee
Sebastian Duarte <sebasnsd13@gmail.com>
2023-07-20 00:17:15 -0300

    jueves

---
+++ stock.js
@@ -0,0 +1,83 @@
+const productosFerre =
+[
+    {
+        id: 1,
+        nombre: "Clavos 1 pulgada",
+        tipo: "clavos",
+        descripcion: "Clavo punta paris por kg",
+        precio: 1900,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Martillo Encofrador",
+        tipo: "martillo",
+        descripcion: "Martillo encofrador con mango de fibra",
+        precio: 3000,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Pinza 7",
+        tipo: "pinza",
+        descripcion: "Pinza de 7 pulgadas con mango revestido",
+        precio: 2900,
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Bisagra 3 Pulgadas Bronce",
+        tipo: "bisagra",
+        descripcion: "Bisagra 3 pulgadas color bronce",
+        precio: 1600,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Bisagra 2 Pulgadas Bronce",
+        tipo: "bisagra",
+        descripcion: "Bisagra 2 pulgadas color bronce",
+        precio: 1400,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "Clavos 2 pulgadas",
+        tipo: "clavos",
+        descripcion: "Clavos 2 pulgadas punta paris",
+        precio: 1500,
+        cantidad:0
+    },
+
+    {
+        id: 7,
+        nombre: "Amoladora Makita 1100W",
+        tipo: "amoladora",
+        descripcion: "Amoladora makita 1100w disco 115mm",
+        precio: 45000,
+        cantidad:0
+    },
+
+    {
+        id: 8,
+        nombre: "Taladro Hamilton",
+        tipo: "taladro",
+        descripcion: "Taladro Hamilton 900w",
+        precio: 25000,
+        cantidad:0
+    },
+
+    {
+        id: 9,
+        nombre: "Serrucho Carpintero 20",
+        tipo: "serrucho",
+        descripcion: "Serrucho carpintero 20 pulgadas mango de pvc",
+        precio: 8000,
+        cantidad:0
+    }
+]
